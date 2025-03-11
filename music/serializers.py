d0b3b66523dbe71f30f3b092582d75ffb8f8791e
NellitG <kamaunelson157@gmail.com>
2025-03-11 18:28:46 +0300

    input youtube validation

--- music/serializers.py
+++ music/serializers.py
@@ -8,10 +8,16 @@ class ArtistSerializer(serializers.ModelSerializer):#serializes model instance
 
 class SongSerializer(serializers.ModelSerializer):
     artist = ArtistSerializer()
+    youtube_url =serializers.CharField()
 
     class Meta:
         model = Song
         fields = ["id", "title", "artist", "youtube_url", "duration", "uploaded_at"]
+
+    def validate_youtube_url(self, value):
+        if not value.startswith("https://www.youtube.com/watch"):
+            raise serializers.ValidationError("Invalid Youtube URL")
+        return value
 
 class PlaylistSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source="user.username")
