e0ba5ed53bf30f3fe0609ee5bd881f5a5f81acf9
NellitG <kamaunelson157@gmail.com>
2025-03-17 12:19:33 +0300

    registered models in my admin

--- music/admin.py
+++ music/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Artist, Song, Playlist
+
+admin.site.register(Artist)
+admin.site.register(Song)
+admin.site.register(Playlist)
